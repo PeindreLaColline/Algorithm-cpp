efac8028ff10860fc2e359331134f993407ef59d
Peindre La Colline <158552381+PeindreLaColline@users.noreply.github.com>
2024-04-24 05:48:46 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 548 ms, Memory: 8176 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int tmp;
+vector<int> v;
+
+void binary(int s, int e) {
+	int mid, f, b;
+	while (s <= e) {
+		mid = (s + e) / 2;
+		f = mid - 1;
+		b = mid + 1;
+		if (v[mid] == tmp) return;
+		if (f >= 0) {
+			if (v[f] < tmp && tmp < v[mid]) {
+				v[mid] = tmp;
+				return;
+			}
+		}
+		if (b <= v.size() - 1) {
+			if (v[mid] < tmp && tmp < v[b]) {
+				v[b] = tmp;
+				return;
+			}
+		}
+		if (v[mid] > tmp) e = mid - 1;
+		else if (v[mid] < tmp) s = mid + 1;
+	}
+	v[0] = tmp;
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	cin >> tmp;
+	v.push_back(tmp);
+
+	for (int i = 0; i < n-1; i++) {
+		cin >> tmp;
+		if (v[v.size() - 1] < tmp) {
+			v.push_back(tmp);
+			continue;
+		}
+		else {
+			binary(0, v.size() - 1);
+		}
+	}
+	cout << v.size();
+}
