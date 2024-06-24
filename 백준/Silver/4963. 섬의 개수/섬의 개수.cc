a47e4873240163f00ca24e94664e87dee02e311b
Peindre La Colline <158552381+PeindreLaColline@users.noreply.github.com>
2024-06-24 16:41:08 +0900

    [Silver II] Title: 섬의 개수, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.cc
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.cc
@@ -1,64 +1,24 @@
-//bfs
+//dfs
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
 int w, h;
 vector<vector<int>> map;
 vector<vector<bool>> visited;
-queue<pair<int, int>> togo;
 
 int dir[8][8] = { {0, -1}, {1, -1}, {1, 0}, {1, 1},
 					{0, 1}, {-1, 1}, {-1, 0}, {-1, -1} };
 int island = 0;
 
-int tx, ty;
-
-pair<int, int> discover() {
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			if (visited[i][j] == false) {
-				if (map[i][j] == 1) {
-					tx = j; ty = i;
-					return { j, i };
-				}
-			}
+void dfs(int x, int y) {
+	for (int i = 0; i < 8; i++) {
+		int nx = x + dir[i][0];
+		int ny = y + dir[i][1];
+		if (nx >= 0 && nx < w && ny >= 0 && ny < h && map[ny][nx] && !visited[ny][nx]) {
+			visited[ny][nx] = true;
+			dfs(nx, ny);
 		}
-	}
-	return { -1, -1 };
-}
-
-void bfs() {
-	while (true) {
-		pair<int, int> tmp = discover();
-		if (tmp.first == -1) break;
-
-		togo.push({ tmp.first, tmp.second });
-		visited[tmp.second][tmp.first] = true;
-		island++;
-
-		while (!togo.empty()) {
-
-			int cx = togo.front().first;
-			int cy = togo.front().second;
-
-			togo.pop();
-
-			for (int i = 0; i < 8;i++) {
-				int nx = cx + dir[i][0];
-				int ny = cy + dir[i][1];
-
-				if (nx < 0 || nx >= w || ny < 0 || ny >= h) {
-					continue;
-				}
-				if (visited[ny][nx]) continue;
-				if (map[ny][nx] == 0) continue;
-				visited[ny][nx] = true;
-				togo.push({ nx,ny });
-			}
-		}
-		
 	}
 }
 
@@ -72,10 +32,6 @@ int main() {
 		map.clear();
 		visited.clear();
 		island = 0;
-		while (!togo.empty()) {
-			togo.pop();
-		}
-		tx = 0; ty = 0;
 
 		cin >> w >> h;
 		if (w == 0 && h == 0) break;
@@ -89,11 +45,16 @@ int main() {
 				cin >> map[i][j];
 			}
 		}
-		
-		bfs();
+
+		for (int i = 0;i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if (map[i][j] == 1 && !visited[i][j]) {
+					visited[i][j] = true;
+					dfs(j, i);
+					island++;
+				}
+			}
+		}
 		cout << island << "\n";
-
 	}
-	
-
 }
