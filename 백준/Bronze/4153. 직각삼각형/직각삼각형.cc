81ddec816a1334f16671771510cf2d7846d3e684
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-03-19 21:37:56 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,22 @@
+/*21:33*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main(){
+    vector<int> v(3);
+    while(true){
+        for(int i =0; i<3; i++){
+            cin >> v[i];
+        }
+        if(v[0] == 0) break;
+
+        sort(v.begin(), v.end());
+
+        if(pow(v[2], 2) == pow(v[0], 2) + pow(v[1], 2)) cout << "right" <<"\n";
+        else cout << "wrong" <<"\n";
+
+    }
+}
