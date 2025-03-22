46eff0f02f4ae69b2ca3065c31fcc7a7b17a6cab
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-03-22 13:49:39 +0900

    Update 소수 구하기.cc

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,7 +1,6 @@
 /*13:30*/
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
 #define LIMIT 1000000
@@ -9,6 +8,11 @@ using namespace std;
 int m, n;
 vector<bool> visited(LIMIT+1, false);
 
+/*
+2부터 LIMIT까지 순회하는데 아직 방문처리 안 된 수는 소수로 판단
+그 수를 가지고 LIMIT까지 순회하며 그 수의 모든 배수를 방문처리 함
+예를 들어 2를 제외한 2의 배수는 전부 방문처리 (4, 6, 8, ...)
+*/
 void prime(){
     for(int i =2; i<=n; i++){
         if(visited[i]) continue;
@@ -23,37 +27,14 @@ void prime(){
 int main(){
     cin >> m >> n; // m <= n
     prime();
+
+    /*
+    visited 배열 순회하면서 visited[i] == false 인 것만 출력 (i는 m과 n 사이)
+    이때 m이 1이면 하나 증가해서 2부터 조회하는데 이는 1이 소수가 아니기 때문
+    */
     if(m==1) m++;
     for(int i =m; i<=n; i++){
         if(visited[i]) continue;
         cout << i << "\n";
     }
 }
-
-// struct compare{
-//     bool operator()(const int &a, const int &b){
-//         return a < b;
-//     }
-// };
-
-// int m, n;
-// priority_queue<int, vector<int>, compare> pq;
-// vector<bool> visited(LIMIT+1, false);
-
-// void prime(){
-//     while(!pq.empty()){
-//         int num = pq.top();
-
-//         while(num<=LIMIT){
-            
-//         }
-//     }
-    
-// }
-
-// int main(){
-//     cin >> n >> m;
-//     for(int i =1; i<= LIMIT; i++){
-//         pq.push(i);
-//     }
-// }
