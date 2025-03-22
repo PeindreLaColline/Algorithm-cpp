425bb480c6e706c927a10b22dcd79864e891e244
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-03-22 14:16:44 +0900

    Update and rename 연산자 끼워넣기.cc to 연산자 끼워넣기(2).cc

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기(2).cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기(2).cc
@@ -6,9 +6,14 @@ using namespace std;
 
 int n;
 vector<int> v;
-int maxi= -1000000000;
+int maxi= -1000000000; //-1e9로 쓰면 double에서 int 변환과정에 오류나기 때문에 숫자 그대로 씀
 int mini= 1000000000;
 
+/*
+oper에는 각 연산자마다 몇번 사용할 수 있는지가 저장 돼 있음
+각 연산자의 사용 횟수가 남아있으면 그 연산자를 사용해서 계산하고 back 다시 호출해서 또 계산
+반복해서 마지막 숫자까지 계산 완료하면 max min값 업데이트
+*/
 void back(vector<int> oper, int num, int idx){
     if(idx == n){
         mini = min(mini, num);
