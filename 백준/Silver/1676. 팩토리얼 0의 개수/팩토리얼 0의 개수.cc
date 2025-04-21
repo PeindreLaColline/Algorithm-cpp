be9048ab65e6459c20298ba9304e8fc15e1907ba
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-04-21 16:01:02 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,16 @@
+/*15:36*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int count = 0;
+    for (int i = 5; i <= n; i *= 5) {
+        count += n / i;
+    }
+
+    cout << count << endl;
+    return 0;
+}
