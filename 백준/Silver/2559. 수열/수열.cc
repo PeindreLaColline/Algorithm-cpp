ccf475c4dc2ba0f46fabba765475073b7e0c831f
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-04-23 01:50:48 +0900

    [Silver III] Title: 수열, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,30 @@
+/*01:43*/
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int n, k;
+vector<int> v;
+
+int main(){
+    cin >> n >> k;
+    v.resize(n);
+    for(int i =0; i<n; i++){
+        cin >> v[i];
+    }
+
+    int cur = 0;
+    for(int i =0; i<k; i++){
+        cur += v[i];
+    }
+    int ans = cur;
+
+    for(int i =1; i<=n-k; i++){
+        cur = cur-v[i-1]+v[i+k-1];
+        ans = max(ans, cur);
+    }
+
+    cout << ans;
+
+}
