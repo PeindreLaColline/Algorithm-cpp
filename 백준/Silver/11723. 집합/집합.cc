87dc9711cee2096ab647c6c15b6c109feabc1750
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-04-28 11:29:10 +0900

    [Silver V] Title: 집합, Time: 592 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,51 +1,49 @@
-/*16:02*/
+/*11:22*/
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 
 int main(){
+
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
     int n;
     cin >> n;
-    vector<bool> v(21, false);
-    vector<bool> v_all(21, true);
-    vector<bool> v_empty(21, false);
 
-    string tmp;
+    string s;
+    vector<bool> set(21, false);
+    vector<bool> all(21, true);
+    vector<bool> emp(21, false);
+
     int num;
     for(int i =0; i<n; i++){
+        cin >> s;
 
-        cin >> tmp;
-        if(tmp == "all"){
-            v = v_all;
+        if(s == "add"){
+            cin >> num;
+            set[num] = true;
         }
-        else if(tmp == "empty"){
-            v = v_empty;
+        else if(s=="remove"){
+            cin >> num;
+            set[num] = false;
+        }
+        else if(s=="check"){
+            cin >> num;
+            cout << set[num] << "\n";
+        }
+        else if(s=="toggle"){
+            cin >> num;
+            set[num] = !set[num];
+        }
+        else if(s=="all"){
+            set = all;
         }
         else{
-            cin >> num;
-
-            if(tmp == "add"){
-                v[num] = true;
-            }
-            else if(tmp == "remove"){
-                v[num] = false;
-            }
-            else if(tmp == "check"){
-                if(v[num]){
-                    cout << 1 << "\n";
-                }
-                else{
-                    cout << 0 << "\n";
-                }
-            }
-            else if(tmp == "toggle"){
-                v[num] = !v[num];
-            }
+            set = emp;
         }
     }
+
 }
