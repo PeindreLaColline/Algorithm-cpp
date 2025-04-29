077042699fa9c39f4fee9ca0d71d28fe438f81dd
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-04-29 12:17:22 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,18 @@
+/*12:13*/
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int k, q, l, b, n, p;
+
+    cin >> k >> q >> l >> b >> n >> p;
+
+    cout << 1-k << " ";
+    cout << 1-q << " ";
+    cout << 2-l << " ";
+    cout << 2-b << " ";
+    cout << 2-n << " ";
+    cout << 8-p << " ";
+}
