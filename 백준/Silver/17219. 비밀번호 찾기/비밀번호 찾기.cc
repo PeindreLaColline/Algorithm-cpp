fb75a3a528dceaf7e2f004d7a7b3363f5d61b692
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-05-02 02:10:16 +0900

    Update 비밀번호 찾기.cc

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -6,13 +6,14 @@
 using namespace std;
 
 int n, m;
-map<string, string> note;
+map<string, string> note; //map으로 사이트랑 비밀번호 저장
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
+
+    //input
     cin >> n >> m;
 
     string site, pw;
@@ -22,6 +23,7 @@ int main(){
         note[site] = pw;
     }
 
+    //output
     for(int i =0; i<m; i++){
         cin >> site;
         cout << note[site] <<"\n";
