6e44228462990e7323fa0eaf00b68a67d8ba2630
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-05-20 11:08:59 +0900

    Update 색종이 만들기.cc

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -4,6 +4,9 @@
 #include <queue>
 using namespace std;
 
+//구조체
+//x, y는 조사할 색종이의 왼쪽 위 좌표
+//size는 조사할 색종이의 크기
 struct info{
     int x;
     int y;
@@ -17,6 +20,7 @@ vector<vector<int> > v;
 int white = 0;
 int blue = 0;
 
+//정해진 구역 내의 색종이 색이 다 같은지 확인
 bool check(info info){
     int x = info.x;
     int y = info.y;
@@ -33,6 +37,8 @@ bool check(info info){
     return true;
 }
 
+//현재 구역의 색종이 색이 전부 동일하지 않다면 4등분해서 queue에 저장
+//동일하다면 blue 혹은 white에 카운트
 void dfs(){
     queue<info> q;
     q.push(info(1, 1, n));
@@ -62,6 +68,7 @@ int main(){
     v.resize(n+1, vector<int>(n+1));
 
     //흰색0, 파란색1
+    //좌표 보기 편하게 하려고 1부터 시작
     for(int i =1; i<=n; i++){
         for(int j = 1; j<=n; j++){
             cin >> v[i][j];
