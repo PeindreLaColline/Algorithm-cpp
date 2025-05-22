6a00bd81f7ff328e1d2850291672d7a3fc6e5ae2
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-05-22 11:54:47 +0900

    [Silver I] Title: IOIOI, Time: 36 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -0,0 +1,66 @@
+/*10:47*/
+    /*
+    case 1 (n==1)
+    OOIOIOIOIIOII
+    0012345671231
+    (7-n)/2 + (3-n)/2 = 3 + 1 = 4
+
+    case 2 (n==2)
+    OOIOIOIOIIOII
+    0012345671231
+    (7-n)/2 + (3-n)/2 = 2 + 0 = 2
+    */
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int n, m;
+string s;
+vector<int> v;
+
+int main(){
+    cin >> n >> m;
+    cin >> s;
+
+    int tmp;
+    bool aie;
+    if(s[0] == 'I'){
+        aie = true;
+        tmp = 1;
+    } 
+    else{
+        aie = false;
+        tmp = 0;
+    }
+
+    int ans = 0;
+    int cnt = 1 + n*2;
+    for(int i = 1; i<s.size(); i++){
+        if(aie){
+            if(s[i] == 'I'){
+                aie = true;
+                tmp = 1;
+            }
+            else{
+                aie = false;
+                tmp++;
+            }
+        }
+        else{
+            if(s[i] == 'I'){
+                aie = true;
+                tmp++;
+                if(tmp >= cnt) ans++;
+            }
+            else{
+                aie = false;
+                tmp = 0;
+            }
+        }
+        
+    }
+
+    cout << ans;
+
+}
