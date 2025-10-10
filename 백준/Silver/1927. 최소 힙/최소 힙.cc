913f44e0ab003825d893f64091ac8073cbeb5b36
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-10-10 17:42:59 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,28 +1,38 @@
+//17:37
 #include <iostream>
+#include <vector>
 #include <queue>
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	priority_queue<int, vector<int>, greater<int> > pq;
-	int n;
-	cin >> n;
-	int buf;
-	for (int i = 0; i < n; i++) {
-		cin >> buf;
-		if (buf == 0) {
-			if (pq.empty()) {
-				cout << 0 << "\n";
-			}
-			else {
-				cout << pq.top() << "\n";
-				pq.pop();
-			}
-		}
-		else {
-			pq.push(buf);
-		}
-	}
+int n;
+
+struct compare{
+    bool operator()(const int &a, const int &b){
+        return a>b;
+    }
+};
+priority_queue<int, vector<int>, compare> pq;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
+    int tmp;
+    for(int i =0; i<n; i++){
+        cin >> tmp;
+        if(tmp > 0){
+            pq.push(tmp);
+        }
+        else if(tmp == 0){
+            if(pq.empty()) cout << 0 << "\n";
+            else{
+                cout << pq.top() <<"\n";
+                pq.pop();
+            }
+        }
+
+    }
 }
