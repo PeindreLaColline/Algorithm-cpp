9c98160d426ea3ffaf5bcfb77e94cd9943db7f0b
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-10-14 21:21:31 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 324 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,33 +1,53 @@
+//20:51
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-#define MAX 10000001
+int n, m;
+int num;
+vector<int> card;
 
-vector<int> pl(MAX, 0);
-vector<int> mi(MAX, 0);
+int binary(int num){
+    int s = 0;
+    int e = n-1;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+    while(s <= e){
+        int mid = (s+e)/2;
 
-	int n, m;
-	cin >> n;
+        if(card[mid] < num) s = mid +1;
+        else if(card[mid] > num) e = mid -1;
+        else return mid;
+    }
 
-	int tmp;
-	for (int i = 0; i < n; i++) {
-		cin >> tmp;
-		if (tmp >= 0) pl[tmp]++;
-		else mi[abs(tmp)]++;
-	}
+    return -1;
+}
 
-	cin >> m;
-	int target;
-	for (int i = 0; i < m; i++) {
-		cin >> target;
-		if (target >= 0) cout << pl[target] << ' ';
-		else cout << mi[abs(target)] << ' ';
-	}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    card.resize(n);
+    for(int i =0; i<n; i++){
+        cin >> card[i];
+    }
+    sort(card.begin(), card.end());
+
+    cin >> m;
+    int cnt;
+    for(int i =0; i<m; i++){
+        cin >> num;
+        int idx = binary(num);
+        if(idx != -1){
+            cnt = upper_bound(card.begin(), card.end(), card[idx]) - lower_bound(card.begin(), card.end(), card[idx]);
+            cout << cnt << " ";
+        }
+        else{
+            cout << 0 << " ";
+        }
+        
+    }
+
 }
