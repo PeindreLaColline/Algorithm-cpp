5c9d395a45ff365c042d4b89929784dbe539dece
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-11-11 18:25:43 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,60 @@
+//15:11
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n;
+vector<vector<int> > map;
+vector<vector<vector<int> > > dp;
+
+int main(){
+    cin >> n;
+    map.resize(n, vector<int>(n));
+    dp.resize(n, vector<vector<int> >(n, vector<int>(3, 0)));
+
+    for(int i =0; i<n; i++){
+        for(int j =0; j<n; j++){
+            cin >> map[i][j];
+        }
+    }
+
+    dp[0][1][0] = 1;
+
+    for(int i =0; i<n; i++){
+        for(int j =0; j<n; j++){
+            if(map[i][j] == 1) continue;
+
+            //가로
+            if(j+1<n && map[i][j+1] != 1){
+                dp[i][j+1][0] += dp[i][j][0];
+            }
+            if(i+1<n && j+1<n && map[i+1][j+1] != 1 && map[i][j+1] != 1 && map[i+1][j] != 1){
+                dp[i+1][j+1][2] += dp[i][j][0];
+            }
+
+            //세로
+            if(i+1 < n && map[i+1][j] != 1){
+                dp[i+1][j][1] += dp[i][j][1];
+            }
+            if(i+1<n && j+1<n && map[i+1][j+1] != 1 && map[i][j+1] != 1 && map[i+1][j] != 1){
+                dp[i+1][j+1][2] += dp[i][j][1];
+            }
+
+            //대각선
+            if(j+1<n && map[i][j+1] != 1){
+                dp[i][j+1][0] += dp[i][j][2];
+            }
+            if(i+1 < n && map[i+1][j] != 1){
+                dp[i+1][j][1] += dp[i][j][2];
+            }
+            if(i+1<n && j+1<n && map[i+1][j+1] != 1 && map[i][j+1] != 1 && map[i+1][j] != 1){
+                dp[i+1][j+1][2] += dp[i][j][2];
+            }
+        }
+    }
+
+    cout << dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2];
+    
+
+}
