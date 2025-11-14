c05e14c75b1c446da26316e05563be7d9b12802e
Chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-11-14 15:42:51 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,7 +1,5 @@
-//15:11
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
 int n;
@@ -26,35 +24,19 @@ int main(){
             if(map[i][j] == 1) continue;
 
             //가로
-            if(j+1<n && map[i][j+1] != 1){
-                dp[i][j+1][0] += dp[i][j][0];
-            }
-            if(i+1<n && j+1<n && map[i+1][j+1] != 1 && map[i][j+1] != 1 && map[i+1][j] != 1){
-                dp[i+1][j+1][2] += dp[i][j][0];
-            }
+            if(j+1 <n && map[i][j+1] != 1) dp[i][j+1][0] += dp[i][j][0];
+            if(j+1 <n && i+1<n && map[i][j+1] != 1 && map[i+1][j] != 1 && map[i+1][j+1] != 1) dp[i+1][j+1][2] += dp[i][j][0];
 
             //세로
-            if(i+1 < n && map[i+1][j] != 1){
-                dp[i+1][j][1] += dp[i][j][1];
-            }
-            if(i+1<n && j+1<n && map[i+1][j+1] != 1 && map[i][j+1] != 1 && map[i+1][j] != 1){
-                dp[i+1][j+1][2] += dp[i][j][1];
-            }
+            if(i+1<n && map[i+1][j] != 1) dp[i+1][j][1] += dp[i][j][1];
+            if(j+1 <n && i+1<n && map[i][j+1] != 1 && map[i+1][j] != 1 && map[i+1][j+1] != 1) dp[i+1][j+1][2] += dp[i][j][1];
 
             //대각선
-            if(j+1<n && map[i][j+1] != 1){
-                dp[i][j+1][0] += dp[i][j][2];
-            }
-            if(i+1 < n && map[i+1][j] != 1){
-                dp[i+1][j][1] += dp[i][j][2];
-            }
-            if(i+1<n && j+1<n && map[i+1][j+1] != 1 && map[i][j+1] != 1 && map[i+1][j] != 1){
-                dp[i+1][j+1][2] += dp[i][j][2];
-            }
+            if(j+1<n && map[i][j+1] != 1) dp[i][j+1][0] += dp[i][j][2];
+            if(i+1<n && map[i+1][j] != 1) dp[i+1][j][1] += dp[i][j][2];
+            if(i+1<n && j+1<n && map[i][j+1]!=1 && map[i+1][j] != 1 && map[i+1][j+1] != 1) dp[i+1][j+1][2] += dp[i][j][2];
         }
     }
 
     cout << dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2];
-    
-
 }
