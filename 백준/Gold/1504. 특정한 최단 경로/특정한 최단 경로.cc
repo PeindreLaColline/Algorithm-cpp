5add633a4843aea3f499aa683670bcb4c11852fb
Chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2025-11-15 16:28:18 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 112 ms, Memory: 4536 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,86 +1,88 @@
-//13:40
+//16:07
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <math.h>
 using namespace std;
 
-const int INF = 1e9;
+#define INF 1e9
 
 int n, e;
+int v1, v2;
 vector<vector<int> > map;
 vector<int> dist;
 
 struct compare{
-    bool operator()(const pair<int, int>& a, const pair<int, int>& b){
+    bool operator()(const pair<int, int> &a, const pair<int, int> &b){
         return a.second > b.second;
     }
 };
 
-void get_distance(int start){
-    priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+void get_dist(int start){
+    priority_queue<pair<int, int>, vector<pair<int, int> >, compare> togo;
     dist[start] = 0;
-    pq.push(make_pair(start, 0));
+    togo.push(make_pair(start, 0));
 
-    while(!pq.empty()){
-        int cur = pq.top().first;
-        int cost = pq.top().second;
-        
-        pq.pop();
+    while(!togo.empty()){
+        int cur = togo.top().first;
+        int val = togo.top().second;
+        togo.pop();
 
-        if(cost > dist[cur]) continue;
+        if(val > dist[cur]) continue;
 
         for(int i =1; i<=n; i++){
-            if(cur == i) continue;
+            if(i == cur) continue;
             int next = i;
-            int n_cost = cost + map[cur][next];
-
-            if(n_cost < dist[next]){
-                dist[next] = n_cost;
-                pq.push(make_pair(next, n_cost));
+            int n_val = val+map[cur][next];
+            
+            if(dist[next] > n_val){
+                dist[next] = n_val;
+                togo.push(make_pair(next, n_val));
             }
         }
-    }
 
+    }
 }
 
 int main(){
     cin >> n >> e;
+
     map.resize(n+1, vector<int>(n+1, INF));
 
     int a, b, c;
+
     for(int i = 0; i<e; i++){
         cin >> a >> b >> c;
         map[a][b] = c;
         map[b][a] = c;
     }
 
-    cin >> a >> b;
-    
-    dist.clear();
-    dist.resize(n+1, INF);
-    get_distance(a);
-    int ab = dist[b];
-    int ae = dist[n];
-    
-    dist.clear();
-    dist.resize(n+1, INF);
-    get_distance(1);
-    int sa = dist[a];
-    int sb = dist[b];
+    cin >> v1 >> v2;
 
     dist.clear();
     dist.resize(n+1, INF);
-    get_distance(b);
-    int be = dist[n];
+    get_dist(1);
+    int sv1 = dist[v1];
+    int sv2 = dist[v2];
 
-    int path1 = INF;
-    int path2 = INF;
+    dist.clear();
+    dist.resize(n+1, INF);
+    get_dist(v1);
+    int v1v2 = dist[v2];
 
-    if(sa < INF && ab < INF && be < INF) path1 = sa + ab + be;
-    if(sb < INF && ab < INF && ae < INF) path2 = sb + ab + ae;
+    dist.clear();
+    dist.resize(n+1, INF);
+    get_dist(n);
+    int v1e = dist[v1];
+    int v2e = dist[v2];
 
-    long long ans = min(path1, path2);
-    if(ans >= INF) cout << -1;
-    else cout << ans;
-    
+    int dist1 = INF;
+    int dist2 = INF;
+
+    if(sv1 < INF && v1v2 < INF && v2e < INF) dist1 = sv1 + v1v2 + v2e;
+    if(sv2 < INF && v1v2 < INF && v1e < INF) dist2 = sv2 + v1v2 + v1e;
+
+    int ans = min(dist1, dist2);
+    if(ans < INF) cout << ans;
+    else cout << -1;
 }
